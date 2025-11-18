363dc821f0a8e49df634a2ad2a1f26ae759bcbb1
Yash <ay85166@gmail.com>
2025-11-19 01:17:56 +0530

    Call it a day made a package

--- Java/Lab/15_creating_package/p1/A.java
+++ Java/Lab/15_creating_package/p1/A.java
@@ -1,5 +1,5 @@
 package p1;
-public class main{
+public class A{
     public void m1(){
         System.out.println("A");
     }
@@ -7,5 +7,12 @@ public class main{
 // public class B{
 //     public void m2(){
 //         System.out.println("B");
+//     }
+// }
+
+// class Test{
+//     public static void main(String args[]){
+//         p1.A obj = new p1.A();
+//         obj.m1();
 //     }
 // }
---
+++ Java/Lab/15_creating_package/test.java
@@ -0,0 +1,7 @@
+import p1.*;
+class test{
+    public static void main(String args[]){
+        p1.A obj = new p1.A();
+        obj.m1();
+    }
+}
